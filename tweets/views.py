0c21ffe6486ada3c8e0d04eac159bfe4aeeac091
David DeWulf <dwdewul@gmail.com>
2017-09-10 15:16:36 -0500

    Remove search bar in favor of bootstrap nav search Add search funcationality with django Q

--- tweets/views.py
+++ tweets/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.forms.utils import ErrorList
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from django.views.generic import (DetailView, ListView,
                                   CreateView, UpdateView, DeleteView)
 from .forms import TweetModelForm
@@ -10,9 +11,18 @@ from .mixins import FormUserNeededMixin, UserOwnerMixin
 
 
 class TweetListView(ListView):
-    queryset = Tweet.objects.all()
     template_name = "tweets/list_view.html"
     context_object_name = 'tweets'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = Tweet.objects.all()
+        query = self.request.GET.get("q", None)
+        if query is not None:
+            qs = qs.filter(
+            Q(text__icontains=query) |
+            Q(user__username__icontains=query)
+            )
+        return qs
 
 
 class TweetDetailView(DetailView):
